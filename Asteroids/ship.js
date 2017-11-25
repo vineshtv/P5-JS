85c9afc3858d1b8cc64d272a5587f81244323670
vineshtv <tv.vinesh@gmail.com>
2017-11-26 03:29:23 +0530

    Added the ship

---
+++ Asteroids/ship.js
@@ -0,0 +1,57 @@
+class Ship {
+    constructor() {
+        this.pos = createVector(width/2, height/2);
+        this.vel = createVector(0,0);
+        this.size = 20;
+        this.heading = 0;
+        this.rotation = 0;
+        this.isThrusting = false;
+    }
+    
+    update() {
+        if(this.isThrusting){
+            this.thrust();
+        }
+        this.pos.add(this.vel);
+        this.vel.mult(0.99);
+    }
+    
+    thrust() {
+        var force = p5.Vector.fromAngle(this.heading);
+        force.mult(0.15);
+        this.vel.add(force);
+    }
+    
+    thrusting(b) {
+        this.isThrusting = b;
+    }
+    
+    render() {
+        this.checkEdges();
+        translate(this.pos.x, this.pos.y);
+        rotate(this.heading + PI/2);
+        stroke(255);
+        noFill();
+        triangle(-this.size, this.size, this.size, this.size, 0, -this.size);
+        if (this.isThrusting){
+            fill(255,0,0, 150);
+        }
+        rect(-this.size/2 , this.size, this.size, this.size/4);
+    }
+    
+    setRotation(angle) {
+        this.rotation = angle;
+    }
+    
+    checkEdges() {
+        if (this.pos.x > width + this.size) {this.pos.x = -this.size}
+        else if (this.pos.x < 0 - this.size) {this.pos.x = width + this.size}
+        
+        if (this.pos.y > height + this.size) {this.pos.y = -this.size}
+        else if (this.pos.y < 0 - this.size) {this.pos.y = height + this.size}
+    }
+    
+    turn() {
+        this.heading += this.rotation;
+    }
+}
--- Asteroids/sketch.js
+++ Asteroids/sketch.js
@@ -1,7 +1,35 @@
+var ship;
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+    ship = new Ship(); 
 }
 
 function draw() {
+    background(0);
+    ship.render();
+    ship.turn();
+    ship.update();
+}
 
+function keyPressed() {
+    if(keyCode == RIGHT_ARROW){
+        ship.setRotation(0.1);
+    } else if (keyCode == LEFT_ARROW){
+        ship.setRotation(-0.1);
+    } else if (keyCode == UP_ARROW) {
+        ship.thrusting(true);
+    }
+}
+
+function keyReleased(){
+    ship.setRotation(0);
+    if(keyIsDown(LEFT_ARROW)){
+        ship.setRotation(-0.1);
+    }else if (keyIsDown(RIGHT_ARROW)){
+        ship.setRotation(0.1);
+    }
+    if(!keyIsDown(UP_ARROW)){
+        ship.thrusting(false);
+    }
+    
 }
